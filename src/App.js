3aa9ac791aa9c287305bed730fd3e1cca98aceb3
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-14 23:23:13 -0400

    import react firebase hooks for user authentication and data store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
+// firebase
 import firebase from "firebase/app"; // sdk
 import "firebase/firestore"; // database
 import "firebase/auth"; // authentication
+// react-firebase-hooks
+import { useAuthState } from "reacct-firebase-hooks/auth";
+import { useCollectionData } from "reacct-firebase-hooks/firestore";
 
 function App() {
   return (
