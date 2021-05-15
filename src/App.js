1737e32d1ecb50fe8c2c98a0ae962ff645a96b58
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-15 00:24:46 -0400

    initialize firebase application

--- src/App.js
+++ src/App.js
@@ -4,19 +4,30 @@ import firebase from "firebase/app"; // sdk
 import "firebase/firestore"; // database
 import "firebase/auth"; // authentication
 // react-firebase-hooks
-import { useAuthState } from "reacct-firebase-hooks/auth";
-import { useCollectionData } from "reacct-firebase-hooks/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 firebase.initializeApp({
-  //
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`,
 });
 
-function App() {
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h2>Welcome</h2>
+      </header>
     </div>
   );
-}
+};
 
 export default App;
